4b29048d689b43a93b36ac7a5d009d15cfcf7152
Yan <yhor450@gmail.com>
2023-04-11 16:46:48 +0300

    init footer

---
+++ src/app/modules/footer/components/footer-telephone/footer-telephone.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+const FooterTelephone: FC = () => {
+  return (
+    <div>
+      <h4>Дзвонити</h4>
+      <p>+380</p>
+    </div>
+  );
+};
+
+export default FooterTelephone;
